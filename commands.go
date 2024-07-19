666c40b8e1c32172fa99b2bf4149c75c5eb2c60d
mw4rf <mw4rf@sent.at>
2024-07-19 19:52:43 +0200

    Better help output

--- commands.go
+++ commands.go
@@ -8,11 +8,19 @@ import (
 // Command: help
 // (Default if no command is provided)
 // Print the help message
+// PrintHelp prints the usage and commands with aligned columns and colors
+// PrintHelp prints the usage and commands with aligned columns and colors
 func PrintHelp() {
-	fmt.Println("Usage: gogit <command> [args]")
-	fmt.Println("Commands:")
-	fmt.Println("  genrepos <root> - Generate and print a JSON string with the details of all git repositories in a given root folder")
-	fmt.Println("  help - Print this help message")
+	fmt.Println(ColorOutput(ColorYellow, "Usage: gogit <command> [arguments]"))
+	fmt.Println(ColorOutput(ColorYellow, "Commands:"))
+
+	// Define the widths for each field
+	commandWidth := 30
+
+	fmt.Printf("  %-*s %s\n", commandWidth, ColorOutput(ColorCyan, "list"), "List the repositories in a simple and compact format")
+	fmt.Printf("  %-*s %s\n", commandWidth, ColorOutput(ColorCyan, "list full"), "List the repositories in a detailed format")
+	fmt.Printf("  %-*s %s\n", commandWidth, ColorOutput(ColorCyan, "genrepos [root]"), "Generate and print a JSON string with the details of all git repositories in a given root folder")
+	fmt.Printf("  %-*s %s\n", commandWidth, ColorOutput(ColorCyan, "help"), "Print this help message")
 }
 
 // Command: genrepos
